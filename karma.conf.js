01a8910b82f8c1b4a2767521bed6d60803da2a2b
Akash Goswami <akash@fusioncharts.com>
2017-12-07 19:05:44 +0530

    FSB-138: Enable preprcessing to get sourcemap when debug unit test

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,22 @@
+const isDebug = arg => arg === '--debug';
+let coverageRules = [
+    {
+        test: /\.js$|\.jsx$/,
+        use: {
+            loader: 'istanbul-instrumenter-loader',
+            options: { esModules: true },
+        },
+        enforce: 'pre',
+        exclude: /node_modules|src\/renderer\/|\.spec\.js$/,
+    }
+    ],
+    preprocessors = ['webpack'];
+
+if (process.argv.some(isDebug)) {
+    coverageRules = [];
+    preprocessors.push('sourcemap');
+}
+
 module.exports = function (config) {
     config.set({
         basePath: './',
@@ -18,39 +37,28 @@ module.exports = function (config) {
                         },
                         exclude: /node_modules/,
                     },
-                    {
-                        test: /\.js$|\.jsx$/,
-                        use: {
-                            loader: 'istanbul-instrumenter-loader',
-                            options: { esModules: true },
-                        },
-                        enforce: 'pre',
-                        exclude: /node_modules|src\/renderer\/|\.spec\.js$/,
-                    },
+                    ...coverageRules
                 ],
             },
+            devtool: 'inline-source-map',
         },
         preprocessors: {
-            'test.webpack.js': ['webpack'],
+            'test.webpack.js': preprocessors
         },
         exclude: [
             '**/*.swp',
         ],
-
         coverageIstanbulReporter: {
             dir: 'coverage/',
             thresholds: {
-                // set to `true` to not fail the test command when thresholds are not met
-                emitWarning: false,
-                // thresholds for all files
-                global: {
+                emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
+                global: { // thresholds for all files
                     statements: 80,
                     lines: 80,
                     branches: 80,
                     functions: 80,
                 },
-                // thresholds per file
-                each: {
+                each: { // thresholds per file
                     statements: 80,
                     lines: 80,
                     branches: 80,
@@ -65,24 +73,26 @@ module.exports = function (config) {
             reports: ['html', 'lcov', 'text-summary'],
             fixWebpackSourcePaths: true,
             reporters: [
-        { type: 'text' },
-        { type: 'html', subdir: 'report-html', file: 'report.html' },
-        { type: 'lcov', subdir: 'report-lcov', file: 'report.txt' },
+                { type: 'text' },
+                { type: 'html', subdir: 'report-html', file: 'report.html' },
+                { type: 'lcov', subdir: 'report-lcov', file: 'report.txt' },
             ],
         },
 
-        reporters: ['progress', 'coverage-istanbul'],
+        reporters: ['spec', 'coverage-istanbul'],
+        specReporter: {
+            maxLogLines: 5, // limit number of lines logged per test
+            suppressErrorSummary: true, // do not print error summary
+            suppressFailed: false, // do not print information about failed tests
+            suppressPassed: false, // do not print information about passed tests
+            suppressSkipped: true, // do not print information about skipped tests
+            showSpecTiming: false, // print the time elapsed for each spec
+        },
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['ChromeHeadlessNoSandbox'],
-        customLaunchers: {
-            ChromeHeadlessNoSandbox: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox']
-            }
-        },
+        browsers: ['ChromeHeadless'],
         singleRun: true,
         concurrency: Infinity,
     });
